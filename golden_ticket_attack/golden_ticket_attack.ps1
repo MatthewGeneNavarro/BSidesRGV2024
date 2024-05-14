cd12fbf3d3e4dfa0b4e9ef01944027364b11e2f1
Johannes Pangrestu <pangrestuj@yahoo.com>
2024-05-14 09:26:00 -0500

    golden_ticket_attack

---
+++ golden_ticket_attack/golden_ticket_attack.ps1
@@ -0,0 +1,23 @@
+﻿# Retrieve eventdata from the EVTX file
+$events_4769 = .\golden_ticket_attack\events_4769.ps1 | Select-Object Id, AccountInformation
+$events_4768 = .\golden_ticket_attack\events_4768.ps1 | Select-Object Id, AccountInformation
+$events_4672 = .\golden_ticket_attack\events_4672.ps1 | Select-Object Id, AccountInformation
+$events_4624 = .\golden_ticket_attack\events_4624.ps1 | Select-Object Id, AccountInformation
+
+# Find the matching account information
+$matchingAccounts = $events_4769 | Where-Object { $events_4768.AccountInformation -contains $_.AccountInformation }
+
+foreach ($account in $matchingAccounts) {
+    # Check if the account information exists in events_4624
+    if ($account.AccountInformation -in $events_4624.AccountInformation) {
+        $accountStatus = "No golden ticket made for low priv users"
+    } else {
+        $accountStatus = "Username does not have an event 4624"
+    }
+    $isHighPrivilege = $events_4672.AccountInformation -contains $account.AccountInformation
+    
+    $account | Add-Member -MemberType NoteProperty -Name "Status" -Value $accountStatus
+    $account | Add-Member -MemberType NoteProperty -Name "HighPrivilegeAccount" -Value $isHighPrivilege
+}
+
+$matchingAccounts | Select-Object AccountInformation, HighPrivilegeAccount, Status | Format-Table
---
+++ kerberos_attack/detect_kerberoasting_attacks.ps1
@@ -0,0 +1,27 @@
+﻿# Retrieve eventdata from the EVTX file
+$events = Get-WinEvent -Path .\kerberos_attack\event4769_log.evtx | Where-Object { $_.Id -eq 4769} | Select-Object *
+
+# Iterate through each event and parse the Message property to extract Ticket Encryption Type
+foreach ($event in $events) {
+    $userName = $event.Message -replace "(?ms).*account Name:\s*(.*?)\s+.*", '$1'
+    $status = $event.Message -replace "(?ms).*Failure Code:\s*(.*?)\s+.*", '$1'
+    $ticketEncryptionType = $event.Message -replace "(?ms).*Ticket Encryption Type:\s*(.*?)\s+.*", '$1'
+    $serviceName = $event.Message -replace "(?ms).*Service Name:\s*(.*?)\s+.*", '$1'
+    $ticketOptions = $event.Message -replace "(?ms).*Ticket Options:\s*(.*?)\s+.*", '$1'
+    $ipAddress = $event.Message -replace "(?ms).*Client Address:\s*(.*?)\s+.*", '$1'
+
+    $event | Add-Member -MemberType NoteProperty -Name "Username" -Value $userName -Force
+    $event | Add-Member -MemberType NoteProperty -Name "Status" -Value $status -Force
+    $event | Add-Member -MemberType NoteProperty -Name "TicketEncryptionType" -Value $ticketEncryptionType -Force
+    $event | Add-Member -MemberType NoteProperty -Name "ServiceName" -Value $serviceName -Force
+    $event | Add-Member -MemberType NoteProperty -Name "TicketOptions" -Value $ticketOptions -Force
+    $event | Add-Member -MemberType NoteProperty -Name "IpAddress" -Value $ipAddress.Replace("::ffff:","") -Force
+}
+
+$accounts_to_be_checked =
+   $events | 
+       Where-Object Status -eq '0x0' |
+       Where-Object ServiceName -notmatch '\$|krbtgt' |    
+       Where-Object TicketEncryptionType -in ('0x17','0x18')
+
+$accounts_to_be_checked | Select-Object TimeCreated, TimeGenerated, Id, ProviderName, Status, TicketEncryptionType, ServiceName, TicketOptions, IpAddress
