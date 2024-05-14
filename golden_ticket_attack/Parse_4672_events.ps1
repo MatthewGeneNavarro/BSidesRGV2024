a8a1b2ce1bc0eff0481e2f02c152c777911c7c00
MatthewGeneNavarro <85688959+MatthewGeneNavarro@users.noreply.github.com>
2024-05-14 15:09:18 -0500

    Add files via upload

---
+++ golden_ticket_attack/Parse_4672_events.ps1
@@ -0,0 +1,10 @@
+﻿# Retrieve eventdata from the EVTX file
+$events_4672 = Get-WinEvent -Path '..\logs\low_priv_user_gold_ticket.evtx' | Where-Object { $_.Id -in 4672} | Select-Object *
+
+# Iterate through each event and parse the Message property to extract Ticket Encryption Type
+foreach ($event in $events_4672) {
+    $accountInformation = $event.Message -replace "(?ms).*Account Name:\s*(.*?)\s+.*", '$1'
+    $event | Add-Member -MemberType NoteProperty -Name "AccountInformation" -Value $accountInformation.ToLower() -Force
+}
+
+$events_4672 | Select-Object Id, AccountInformation -Unique
